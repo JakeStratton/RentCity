63fc865c3def98f92e21cbf9c30d4b01d1b82f2d
Jake Stratton <jacobcstratton@gmail.com>
2019-06-29 23:21:41 -0500

    crape urls

--- scrape.py
+++ scrape.py
@@ -15,32 +15,65 @@ url = 'https://streeteasy.com/building/parc-east/23h'
 
 # function to get rental info
 def get_rental_info(url):
-    '''creates a pandas dataframe of rental info using beautiful soup to scrape
+     '''creates a pandas dataframe of rental info using beautiful soup to scrape
      from streeteasy.com
 
-    Parameters
-    ----------
-    url: the URL of the rental page to scrape
-    
-    Returns
-    -------
-    rental_single_df: the dataframe of apartment info'''
+     Parameters
+     ----------
+     url: the URL of the rental page to scrape
 
-    # use selenium with chromedriver to get dynamic JS page.  requests library won't wait for JS.
-    chromedriver = "~/Downloads/chromedriver" # path to the chromedriver executable
-    chromedriver = os.path.expanduser(chromedriver)
-    print('chromedriver path: {}'.format(chromedriver))
-    sys.path.append(chromedriver)
-    driver = webdriver.Chrome(chromedriver)
+     Returns
+     -------
+     rental_single_df: the dataframe of apartment info'''
 
-    driver.get(url) #get the page
-    soup = BeautifulSoup(driver.page_source, 'html.parser') #make the soup
+     # use selenium with chromedriver to get dynamic JS page.  requests library won't wait for JS.
+     chromedriver = "~/Downloads/chromedriver" # path to the chromedriver executable
+     chromedriver = os.path.expanduser(chromedriver)
+     print('chromedriver path: {}'.format(chromedriver))
+     sys.path.append(chromedriver)
+     driver = webdriver.Chrome(chromedriver)
 
-    # get rental info
-    rental_info = []
-    rental_info_raw = soup.find_all(defer=True) # gets rental info data by class
-    '''for data in rental_info_raw.find_all('div'):  # for loop to go through rental info
-        rental_info.append(data.get_text()) # add rental info to list'''
+     driver.get(url) #get the page
+     time.sleep(2)
+     soup = BeautifulSoup(driver.page_source, 'html.parser') #make the soup
 
-    return rental_info
+     # get rental info and add raw data to list
+     rental_info_raw = soup.find_all(defer=True) # gets rental info data by finding any tag that has a defer attribute
+     rental_info_string = str(rental_info_raw[0])  # makes a string out of the relevant info
+     rental_info = rental_info_string.split('\n') #creates a list of the raw info
 
+     #clean data
+     rental_info = [i for i in rental_info if i.startswith('googletag')] 
+     rental_info = [x.replace('googletag.pubads().setTargeting(\'', '"') for x in rental_info] 
+     rental_info = [x.replace('\',', '":') for x in rental_info]
+     rental_info = [x.replace('");', '"') for x in rental_info]
+     rental_info = ','.join(rental_info) 
+     rental_info = '{' + rental_info + '}'
+     rental_info = [rental_info]
+
+     return rental_info
+
+
+def get_rental_urls(start_page, end_page):
+     base_url = 'https://streeteasy.com/for-rent/nyc?page='
+
+     # use selenium with chromedriver to get dynamic JS page.  requests library won't wait for JS.
+     chromedriver = "~/Downloads/chromedriver" # path to the chromedriver executable
+     chromedriver = os.path.expanduser(chromedriver)
+     print('chromedriver path: {}'.format(chromedriver))
+     sys.path.append(chromedriver)
+     driver = webdriver.Chrome(chromedriver)
+
+     urls = []
+     for i in range(start_page, end_page):
+          driver.get(base_url + str(i))
+          time.sleep(2)
+          listings = driver.find_elements_by_css_selector('article.item')
+          for listing in listings:
+               item = listing.find_element_by_css_selector('.details-title a').get_attribute('href')
+               urls.append(item)
+
+     return urls
+
+
+          
--- scratch.py
+++ scratch.py
@@ -1,160 +1,67 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd 
-import re
-import ast
-import time 
-from datetime import datetime
+import os
+import time
+import random
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+# chrome_options = Options()
+# chrome_options.add_argument("--headless")
 
-# function to run everything
-def run_scrape():
-    make_game_df()n poor areas
-    return None
+# driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Firefox()
+base_url = 'https://streeteasy.com/for-sale/nyc?page='
 
-# function to get game info
-def get_game_info(url):
-    '''
-    creates a pandas dataframe of game info using beautiful soup to scrape
-     from baseball-reference.com
+def get_page(page=1):
+    # use selenium with chromedriver to get dynamic JS page.  requests library won't wait for JS.
+    chromedriver = "~/Downloads/chromedriver" # path to the chromedriver executable
+    chromedriver = os.path.expanduser(chromedriver)
+    print('chromedriver path: {}'.format(chromedriver))
+    sys.path.append(chromedriver)
+    driver = webdriver.Chrome(chromedriver)
 
-    Parameters
-    ----------
-    url: the URL of the game page to scrape
-    
-    Returns
-    -------
-    game_info_df: the dtaframe of game info
-    '''
+    driver.get(base_url + str(page))
+    time.sleep(60)
+    items = []
+    listings = driver.find_elements_by_css_selector('article.item')
+    for listing in listings:
+        item = {
+            'address': listing.find_element_by_css_selector('.details-title a').text,
+            'id': listing.find_element_by_css_selector('.details-title a').get_attribute('data-gtm-listing-id'),
+            'url': listing.find_element_by_css_selector('.details-title a').get_attribute('href'),
+            'price':  listing.find_element_by_css_selector('.price').text,
+            'monthly':  listing.find_element_by_css_selector('.monthly_payment').text,
+            'details': [d.text.strip() for d in listing.find_elements_by_css_selector('ul.details_info li, li.details_info')],
+            'location': listing.get_attribute('se:map:point'),
+            'payment_details': {}
+        }
 
-    # set page source
-    game_page = requests.get(url) # used url passed through as a parameter
-    soup = BeautifulSoup(game_page.text, 'html.parser')
-
-    # get game info including attendance data
-    game_info = []
-    game_info_raw = soup.find('div', attrs = {'class': 'scorebox_meta'}) # gets game info data by class
-    for data in game_info_raw.find_all('div'):  # for loop to go through game info
-        game_info.append(data.get_text()) # add game info to list
-
-    # remove last three lines of list, unneeded info
-    game_info.pop() 
-    game_info.pop()
-    game_info.pop()
-
-    # standardize format of elements in list by fixing date element
-    game_info[0]='date: '+game_info[0]   
-
-    # add game id 
-    sep1, sep2 = '.', '/'
-    game_id = url.split(sep1)[2]
-    game_id = str('game_id: ' + game_id.split(sep2)[3])
-    game_info = [game_id] + game_info  # add game id to beginning of game info list
-
-    # get runs scored
-    game_runs = []
-    for data in soup.find_all('div', attrs = {'class': 'score'}):  # for loop to go through game runs
-        game_runs.append(data.get_text())    # add game runs to list
-
-
-    # combine game_info and _game_runs and make all the data formatted the same
-    game_info = game_info + game_runs   # combine game_runs and game_info
-    game_info[-1]='home_team_runs: '+game_info[-1] # add formatting to new entries to get the whole list the same
-    game_info[-2]='away_team_runs: '+game_info[-2]
-    game_info = [x.replace(' ', '_') for x in game_info]  # replaces spaces with underscores
-    game_info = [x.replace(':_', '":"') for x in game_info] 
-    game_info = [x.lower() for x in game_info]  # lowercase
-    game_info = ['"' + x + '"' for x in game_info]
-    game_info = ','.join(game_info)
-    game_info = '{' + game_info + '}'
-    game_info = [game_info] 
-
-
-    # convert game_info to a list of dictionaries
-    game_info_dicts = []
-    for i in game_info:
-        game_info_dicts.append(ast.literal_eval(i))
-    
-    game_info_df = pd.DataFrame(game_info_dicts)
-    return game_info_df
-
-
-# function to run scrape,  merge dataframes, return final dataframe
-def make_game_df():
-    '''
-    Begin the scrape process for 2018 MLB game data, 
-    save as csv, and return as a dataframe.
-    
-    Paramters:  None
-
-    '''
-
-    # open the games file
-    with open('/home/jake/data_science/capstones/scoring-attendance/scrape/games20.txt', 'r') as games:
-        raw_text=games.read()
-        while '\n,' in raw_text:  # clean up formatting
-            raw_text=raw_text.replace('\n,', ',')
-
-    game_list = raw_text.split(',')  # create game list
-
-    all_game_info_df = pd.DataFrame()  # create empty dataframe
-
-
-    # open log file
-    scrape_log = open('scrape_log.txt','a')
-
-
-    # loop through urls, check for valid site, if valid append df, log results
-    # timers:  2 sec on success, 4 sec on fail
-    for url in game_list:
         try:
-            game_page = requests.get(url)
-            if game_page.status_code == 200:
-                all_game_info_df = all_game_info_df.append(get_game_info(url), ignore_index=True)
-                result = str(datetime.now()) + ':::' + str(game_page.status_code) + ':::' + str(url)
-                scrape_log.write("\nsuccess %s" % (result))
-                print("success %s" % (result))
-                time.sleep(2) # VERY IMPORTANT - timer delay for get requests - DO NOT DELETE
-            else:
-                result = str(datetime.now()) + ':::' + str(game_page.status_code) + ':::' + str(url) 
-                scrape_log.write("\nFAILURE %s" % (result))
-                print("FAILURE %s" % (result))
-                time.sleep(3) # VERY IMPORTANT - timer delay for get requests - DO NOT DELETE
+            payment_details = listing.find_element_by_css_selector('.EstimateCalculator').get_attribute('se:monthly_payment:attributes')
+            payment_details = json.loads(payment_details)
+            item['payment_details'] = payment_details
+        except:
+            pass
+
+        items.append(item)
+
+    return items
+
+
+def get_all(total_pages=10, start_page=1):
+    items = []
+    for i in range(start_page, total_pages):
+        try:
+            _items = get_page(i)
         except Exception as e:
-            result = str(datetime.now()) + ':::' + str(e) + ':::' + str(url)
-            scrape_log.write('\nEXCEPTION: %s' % (result))
-            print('EXCEPTION: ' + str(e) + result)
-
-    # close log file
-    scrape_log.close()
-
-
-    # final cleanup on dataframe, these issues were discovered during EDA
-    all_game_info_df['attendance'] = all_game_info_df['attendance'].replace({',': ''}, regex=True)
-    all_game_info_df['attendance'] = pd.to_numeric(all_game_info_df['attendance'])
-    all_game_info_df['date'] = all_game_info_df['game_id'].map(lambda x: str(x)[:-1])
-    all_game_info_df['date'] = all_game_info_df['date'].map(lambda x: str(x)[3:])
-    all_game_info_df['date'] = pd.to_datetime(all_game_info_df['date'])
-    all_game_info_df['team_id'] = all_game_info_df['game_id'].map(lambda x: str(x)[:-9])    # add team_id
-    
-    # add capacity column
-    capacity_df = pd.read_csv('/home/jake/data_science/capstones/scoring-attendance/scrape/capacity.csv')
-    all_game_info_df = pd.merge(all_game_info_df, capacity_df, on='team_id', how='outer') 
-    all_game_info_df = all_game_info_df.reset_index()
-
-    
-    # save csv file from dataframe
-    all_game_info_df.to_csv('final_game_info.csv')
+            print(e)
+            _items = []
+        items += _items
+        print(len(items))
+        with open('apartments.json', 'w') as outfile:
+            json.dump(items, outfile, indent=2)
+        time.sleep(random.randint(1, 4))
 
 
-    # return the dataframe
-    return all_game_info_df
-
-
-# for debugging
-
-'''
 if __name__ == '__main__':
-    make_game_df()
-
-    '''
+    get_all(1064, start_page=738)
